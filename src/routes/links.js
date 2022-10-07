e70323661a8c4b5ee4e26cb1ba122f8e74a5b4e7
CAMIRO <juancamilosc@hotmail.com>
2022-10-07 11:25:56 -0500

    settingup the links form

--- src/routes/links.js
+++ src/routes/links.js
@@ -3,4 +3,19 @@ const router = express.Router();
 
 const pool = require("../database");
 
+router.get("/add", (req, res) => {
+  res.render("links/add");
+});
+
+router.post("/add", async (req, res) => {
+  const { title, url, description } = req.body;
+  const newLink = {
+    title,
+    url,
+    description,
+  };
+  await pool.query("INSERT INTO links set ?", [newLink]);
+  res.send("received");
+});
+
 module.exports = router;
