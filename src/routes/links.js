e788133d425e3fcdb7e3f1ed3b723dd913fb4308
CAMIRO <juancamilosc@hotmail.com>
2022-10-08 12:51:26 -0500

    adding the delete and modify functionality

--- src/routes/links.js
+++ src/routes/links.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const pool = require("../database");
 
+// go the the add form
 router.get("/add", (req, res) => {
   res.render("links/add");
 });
 
+// add a new link
 router.post("/add", async (req, res) => {
   const { title, url, description } = req.body;
   const newLink = {
@@ -18,9 +20,37 @@ router.post("/add", async (req, res) => {
   res.redirect("/links");
 });
 
+// get the list of the links
 router.get("/", async (req, res) => {
   const links = await pool.query("SELECT * FROM links");
   res.render("links/list", { links });
+});
+
+// delete a link
+router.get("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  await pool.query("DELETE FROM links WHERE ID = ?", [id]);
+  res.redirect("/links");
+});
+
+// modify a link
+router.get("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  const link = await pool.query("SELECT * FROM links WHERE id = ?", [id]);
+  res.render("links/edit", { link: link[0] });
+});
+
+// modify a link saving the changes
+router.post("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, description, url } = req.body;
+  const newLink = {
+    title,
+    description,
+    url,
+  };
+  await pool.query("UPDATE links set ? WHERE id = ?", [newLink, id]);
+  res.redirect("/links");
 });
 
 module.exports = router;
